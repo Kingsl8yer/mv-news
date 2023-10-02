acf0d0a0d07232ea85eeaa941a6769f567bc262c
mike_varona <migueluk91@gmail.com>
2023-10-02 14:38:23 +0100

    3-GET /api completed

--- MVC/controllers/topicsController.js
+++ MVC/controllers/topicsController.js
@@ -1,9 +1,32 @@
-const {selectAllTopics} = require('../models/topicsModel');
+const { selectAllTopics } = require("../models/topicsModel");
 
 exports.getAllTopics = (req, res, next) => {
   selectAllTopics()
-      .then((topics) => {
-        res.status(200).send({topics});
-      })
-      .catch(next);
-  }
+    .then((topics) => {
+      res.status(200).send({ topics });
+    })
+    .catch(next);
+};
+
+exports.getAllEndpoints = (req, res, next) => {
+  res.status(200).send({
+    endpoints: {
+      "GET /api/topics": {
+        description: "serves an array of all topics",
+        queries: [],
+        exampleResponse: {
+          topics: [
+            {
+              slug: "basketball",
+              description: "Stephen Curry is the best!",
+            },
+            {
+              slug: "coding",
+              description: "Hola mundo!",
+            },
+          ],
+        },
+      },
+    },
+  });
+};
--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -31,3 +31,36 @@ describe("GET /api/topics", () => {
       });
   });
 });
+
+describe("GET /api", () => {
+  test("status 200: responds with an object containing all endpoints", () => {
+    return request(app)
+      .get("/api")
+      .expect(200)
+      .then(({ body }) => {
+        for (let key in body.endpoints) {
+          expect(typeof body.endpoints[key].description).toBe("string");
+          expect(typeof body.endpoints[key].exampleResponse).toBe("object");
+          expect(Array.isArray(body.endpoints[key].queries)).toBe(true);
+          for(let i = 0; i < body.endpoints[key].exampleResponse.topics.length; i++) {
+            expect(
+              typeof body.endpoints[key].exampleResponse.topics[i].slug
+            ).toBe("string");
+            expect(
+              typeof body.endpoints[key].exampleResponse.topics[i].description
+            ).toBe("string");
+          }
+        }
+      });
+  });
+
+  //Error handling
+  test("status 404: responds with error message when given a wrong URL", () => {
+    return request(app)
+      .get("/apii")
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.msg).toBe("URL not found");
+      });
+  });
+});
