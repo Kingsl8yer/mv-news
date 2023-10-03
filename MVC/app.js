a08af35af2a065af7d4e8032650d7bb372b5c52a
mike_varona <migueluk91@gmail.com>
2023-10-03 10:31:09 +0100

    Completed GET /api/articles and its tests

--- MVC/app.js
+++ MVC/app.js
@@ -10,13 +10,15 @@ const {
   getAllTopics,
   getAllEndpoints,
 } = require("./controllers/topicsController");
-const { getArticleById } = require("./controllers/articlesController");
+const { getArticleById, getAllArticles } = require("./controllers/articlesController");
 
 app.get("/api/topics", getAllTopics);
 
 app.get("/api", getAllEndpoints);
 
 app.get("/api/articles/:article_id", getArticleById);
+
+app.get("/api/articles", getAllArticles);
 
 app.all("/*", (req, res, next) => {
   res.status(404).send({ msg: "URL not found" });
@@ -26,5 +28,6 @@ app.all("/*", (req, res, next) => {
 app.use(handleCustomErrors);
 app.use(handlePsqlErrors);
 app.use(handleServerErrors);
+
 
 module.exports = app;
--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -93,3 +93,34 @@ describe("GET /api/articles/:article_id", () => {
 
 });
 
+describe("GET /api/articles", () => {
+  test("status 200: responds with an array of article objects", () => {
+      return request(app)
+      .get("/api/articles")
+      .expect(200)
+      .then(({ body }) => {
+          expect(body.articles.length).toBe(13);
+          body.articles.forEach((article) => {
+              expect(typeof article.author).toBe("string");
+              expect(typeof article.title).toBe("string");
+              expect(typeof article.article_id).toBe("number");
+              expect(typeof article.topic).toBe("string");
+              expect(typeof article.created_at).toBe("string");
+              expect(typeof article.votes).toBe("number");
+              expect(typeof article.article_img_url).toBe("string");
+              expect(typeof article.comment_count).toBe("string");
+          });
+      });
+  });
+
+  //Error handling
+  test("status 404: responds with error message when given a wrong URL", () => {
+      return request(app)
+      .get("/api/article")
+      .expect(404)
+      .then(({ body }) => {
+          expect(body.msg).toBe("URL not found");
+      });
+  });
+});
+
