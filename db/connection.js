c5483015414d7885392fc3b61c2bae32df1d76ae
mike_varona <migueluk91@gmail.com>
2023-10-04 16:20:52 +0100

    Refracted connections.js

--- db/connection.js
+++ db/connection.js
@@ -5,6 +5,11 @@ require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
+
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error('PGDATABASE or DATABASE_URL not set');
+}
+
 const config = {}; 
 
 if (ENV === 'production') {
@@ -13,8 +18,4 @@ if (ENV === 'production') {
 }
 
 
-if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
-  throw new Error('PGDATABASE or DATABASE_URL not set');
-}
-
-module.exports = new Pool();
+module.exports = new Pool(config);
