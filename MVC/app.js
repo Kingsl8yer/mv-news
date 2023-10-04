cc4428d669f340eec63eb9cda4e57288ad61a8f8
mike_varona <migueluk91@gmail.com>
2023-10-04 09:51:54 +0100

    Refracted endpoints and app

--- MVC/app.js
+++ MVC/app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
-app.use(express.json());
 const {
     handleCustomErrors,
     handlePsqlErrors,
@@ -11,7 +10,8 @@ const {
   getAllTopics,
   getAllEndpoints,
 } = require("./controllers/topicsController");
-const { getArticleById, getAllArticles, getCommentsByArticleId,postCommentByArticleId, patchArticleById } = require("./controllers/articlesController");
+const { getArticleById, getAllArticles, getCommentsByArticleId,
+  postCommentByArticleId, patchArticleById } = require("./controllers/articlesController");
 
 app.use(express.json());
 
@@ -24,6 +24,10 @@ app.get("/api/articles/:article_id", getArticleById);
 app.get("/api/articles", getAllArticles);
 
 app.get("/api/articles/:article_id/comments", getCommentsByArticleId);
+
+app.post("/api/articles/:article_id/comments", postCommentByArticleId);
+
+app.patch("/api/articles/:article_id", patchArticleById);
 
 
 app.all("/*", (req, res, next) => {
